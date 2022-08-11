532118ccf2eca1b8bf09fc093ce59ef002735876
s_omenoe <otashi24@gamil.com>
2022-08-11 21:55:20 +0700

    init

---
+++ cipher.js
@@ -0,0 +1,44 @@
+const encoder = salt => {
+  const textToChars = text => text.split('').map(c => c.charCodeAt(0));
+  const byteHex = n => ("0" + Number(n).toString(16)).substr(-2);
+  const applySaltToChar = code => textToChars(salt).reduce((a, b) => a ^ b, code);
+
+  return text => text.split('')
+    .map(textToChars)
+    .map(applySaltToChar)
+    .map(byteHex)
+    .join('');
+}
+
+const decoder = salt => {
+  const textToChars = text => text.split('').map(c => c.charCodeAt(0));
+  const applySaltToChar = code => textToChars(salt).reduce((a, b) => a ^ b, code);
+  return encoded => encoded.match(/.{1,2}/g)
+    .map(hex => parseInt(hex, 16))
+    .map(applySaltToChar)
+    .map(charCode => String.fromCharCode(charCode))
+    .join('');
+}
+
+// create a secret key to decrypt the secret text
+const secretKey = 'SecretTextKey';
+
+// text that you did not want anyone to read
+const targetText = 'the secret string';
+
+// create a secret text generator
+const secretTextGenerator = encoder(secretKey);
+
+// create a secret text
+const secretText = secretTextGenerator(targetText);
+
+// to read the secret text, you need to create a reader that have same secret key to read it
+const secretTextReader = decoder(secretKey);
+
+// use the reader to turn secret text into normal text
+const readableText = secretTextReader(secretText);
+
+console.log('target key    = ', targetText);
+console.log('secret key    = ', secretKey);
+console.log('secret text   = ', secretText);
+console.log('readable text = ', readableText);
